0a4c53a04e5a9f5e39848daa6beddf3f402e0bfe
ahmedali8 <imahmedalibhatti@gmail.com>
2020-12-01 12:56:25 +0500

    added loadBlockchain function and initial state

--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -1,8 +1,15 @@
-import React, { createContext, useReducer, useContext } from 'react';
+import React, { createContext, useReducer, useEffect, useContext } from 'react';
 import AppReducer from './AppReducer';
+import { loadBlockchain } from './asyncActions';
 
 // Initial state
-const initialState = [];
+const initialState = {
+  web3: null,
+  transactions: [],
+  account: '',
+  contract: {},
+  loading: true,
+};
 
 // Create context
 export const GlobalContext = createContext(initialState);
@@ -10,6 +17,10 @@ export const GlobalContext = createContext(initialState);
 // Global provider
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  useEffect(() => {
+    loadBlockchain(dispatch);
+  }, []);
 
   return (
     <GlobalContext.Provider value={[state, dispatch]}>
