47fe26dfd121cf44a8212b76083782604027b347
ahmedali8 <imahmedalibhatti@gmail.com>
2020-12-01 23:12:24 +0500

    IncomeExpenses component complete

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Header } from './Header';
 export { default as AccountInfo } from './AccountInfo';
 export { default as Balance } from './Balance';
+export { default as IncomeExpenses } from './IncomeExpenses';
