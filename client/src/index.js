45dfe6ff9ec9ce0fbecb9223a642dfaa58a6e91b
ahmedali8 <imahmedalibhatti@gmail.com>
2020-12-01 23:57:00 +0500

    added Global provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { GlobalProvider } from './context/GlobalState';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <GlobalProvider>
     <App />
-  </React.StrictMode>,
+  </GlobalProvider>,
   document.getElementById('root')
 );
