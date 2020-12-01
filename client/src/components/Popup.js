f5eeeb3f2e972462500fb41ba36f6f91e9315305
ahmedali8 <imahmedalibhatti@gmail.com>
2020-12-01 23:57:29 +0500

    added Popup component

---
+++ client/src/components/Popup.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Popup = () => {
+  return (
+    <div className="hover_bkgr_fricc">
+      <div>
+        <h5>Loading ...</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Popup;
