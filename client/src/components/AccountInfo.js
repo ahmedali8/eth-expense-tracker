5b86841efb9ced4925e3daee416fe15d13ba0cb5
ahmedali8 <imahmedalibhatti@gmail.com>
2020-12-01 22:36:06 +0500

    Ethereum account info component

---
+++ client/src/components/AccountInfo.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import { useStore } from '../context/GlobalState';
+
+const AccountInfo = () => {
+  const [{ web3, account }] = useStore();
+  const [accountBalance, setAccountBalance] = useState();
+  const [networkType, setNetworkType] = useState();
+
+  console.log(account);
+
+  useEffect(() => {
+    (async () => {
+      if (web3 && account) {
+        let balance = await web3.eth.getBalance(account);
+        balance = web3.utils.fromWei(balance, 'ether');
+        console.log(balance);
+        setAccountBalance(balance);
+
+        let nType = await web3.eth.net.getNetworkType();
+        console.log(nType);
+        setNetworkType(nType);
+      }
+    })();
+  }, [web3, account]);
+  return (
+    <>
+      {web3 && account ? (
+        <>
+          <p>
+            Account: <small>{account}</small>
+          </p>
+          <p>
+            Balance: <small>{accountBalance}</small>
+          </p>
+          <p>
+            Network: <small>{networkType}</small>
+          </p>
+        </>
+      ) : (
+        <div>Cannot load account info</div>
+      )}
+    </>
+  );
+};
+
+export default AccountInfo;
