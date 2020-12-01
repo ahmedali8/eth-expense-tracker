fedfa456df6b90d70b7357eab627ce07a941fa3d
ahmedali8 <imahmedalibhatti@gmail.com>
2020-12-01 23:57:55 +0500

    App component complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,42 @@
 import React from 'react';
-import { GlobalProvider } from './context/GlobalState';
+// global context
+import { useStore } from './context/GlobalState';
+// components
+import {
+  Header,
+  AccountInfo,
+  Balance,
+  IncomeExpenses,
+  TransactionList,
+  AddTransaction,
+  Footer,
+  Popup,
+} from './components';
+// css file
+import './App.css';
 
 function App() {
-  return <GlobalProvider>Hello World</GlobalProvider>;
+  const [{ loading }] = useStore();
+
+  return (
+    <>
+      {loading ? (
+        <Popup />
+      ) : (
+        <div className="box-ui">
+          <Header />
+          <AccountInfo />
+          <div className="container">
+            <Balance />
+            <IncomeExpenses />
+            <TransactionList />
+            <AddTransaction />
+            <Footer />
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default App;
